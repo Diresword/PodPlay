e39809be3002b1d533db0b8a306c987bb4559bc7
Zachary McNatt <zmcnatt@gmail.com>
2021-09-28 22:28:25 -0400

    Error on pg. 528

--- app/src/main/java/com/raywenderlich/podplay/repository/PodcastRepo.kt
+++ app/src/main/java/com/raywenderlich/podplay/repository/PodcastRepo.kt
@@ -1,6 +1,7 @@
 package com.raywenderlich.podplay.repository
 
 import com.raywenderlich.podplay.model.Podcast
+import com.raywenderlich.podplay.service.RssFeedService
 
 class PodcastRepo {
     fun getPodcast(feedUrl: String): Podcast? {
